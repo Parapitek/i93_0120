b511f8b0d9933fe4b92e7e70099cb198e84c780d
Rustam Safarov <safarov.rust@gmail.com>
2022-01-20 12:08:00 +0700

    start

--- main.cpp
+++ main.cpp
@@ -1,15 +1,11 @@
 #include <QCoreApplication>
-#include "student.h"
+
 
 int main(int argc, char *argv[])
 {
     QCoreApplication a(argc, argv);
 
-    Student s1;
-    s1.print();
 
-    Student s2("Petya", 22, 4.5);
-    s2.print();
 
     return a.exec();
 }
